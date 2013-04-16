4b37d138fff63fc89b111f9f3356399284423163
iPodling <podling23@gmail.com>
2013-04-16 19:36:37 +0100

    adding in sidebar stuff and apologising for my spelling

--- src/uk/co/ipodling/newsblurb/Sidebar.java
+++ src/uk/co/ipodling/newsblurb/Sidebar.java
@@ -16,6 +16,19 @@ public class Sidebar extends ViewGroup{
 	// pulls will be marked **, I will note own work and implementation notes to show understanding
 	
     public final static int DURATION = 500; //** used as duration for sidebar animation
+    protected boolean placeLeft = true; //** obviously for basically every variable because, I still need variables
+    protected boolean opened;
+    protected View theSidebar;
+    protected View theContent;
+    protected int sidebarWidth = 150; //default value which is rewritten later
+    
+    protected Animation mAnimation;
+    protected OpenListener    mOpenListener;
+    protected CloseListener   mCloseListener;
+    protected Listener mListener;
+
+    protected boolean mPressed = false;
+
 
 	public Sidebar(Context context) {
 		super(context);
@@ -27,6 +40,55 @@ public class Sidebar extends ViewGroup{
 		// TODO Auto-generated method stub
 		
 	}
+	//here be dragons, and classes to implement
+	class OpenListener implements Animation.AnimationListener {
+
+		@Override
+		public void onAnimationEnd(Animation animation) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void onAnimationRepeat(Animation animation) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void onAnimationStart(Animation animation) {
+			// TODO Auto-generated method stub
+			
+		}
+	}
+	
+    class CloseListener implements Animation.AnimationListener {
+
+		@Override
+		public void onAnimationEnd(Animation animation) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void onAnimationRepeat(Animation animation) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void onAnimationStart(Animation animation) {
+			// TODO Auto-generated method stub
+			
+		}
+
+	}
+    
+    public interface Listener {
+        public void onSidebarOpened();
+        public void onSidebarClosed();
+        public boolean onContentTouchedWhenOpening();
+    }
 
 
 }
